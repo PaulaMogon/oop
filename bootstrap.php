4f43148908e53859b2c6d9c5f1e5c3c4e3d034b9
Paula <paumogon@gmail.com>
2021-07-06 09:08:44 +0200

    BattleResult model class created

--- bootstrap.php
+++ bootstrap.php
@@ -2,4 +2,5 @@
 
 require __DIR__.'/lib/Ship.php';
 require __DIR__.'/lib/ShipLoader.php';
-// require __DIR__.'/lib/BattleManager.php';
+require __DIR__.'/lib/BattleManager.php';
+require_once __DIR__.'/lib/BattleResult.php';
